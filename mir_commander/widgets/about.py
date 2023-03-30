1cd879850219737af969aa01caed4dd7dac4ff69
Yury V. Vishnevskiy <yu.v.vishnevskiy@gmail.com>
2023-03-30 21:04:14 +0200

    Added icon to the About window

--- mir_commander/widgets/about.py
+++ mir_commander/widgets/about.py
@@ -1,4 +1,5 @@
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import QDialog, QLabel, QVBoxLayout
 
 from mir_commander import __version__
@@ -8,9 +9,16 @@ class About(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        self.setWindowTitle("About Mir Commander")
+
         layout = QVBoxLayout()
-        layout.addWidget(QLabel("Mir Commander"), 0, Qt.AlignCenter)
-        layout.addWidget(QLabel(f"Version {__version__}"), 0, Qt.AlignCenter)
+        label = QLabel(self)
+        pixmap = QPixmap("resources/appicon.svg")
+        label.setPixmap(pixmap.scaledToWidth(150, mode=Qt.SmoothTransformation))
+        layout.addWidget(label, 100, Qt.AlignCenter)
+        layout.addWidget(QLabel(f"Mir Commander {__version__}"), 100, Qt.AlignCenter)
+        layout.addWidget(QLabel("Yury V. Vishnevskiy"), 0, Qt.AlignCenter)
+        layout.addWidget(QLabel("Valery V. Vishnevskiy"), 0, Qt.AlignCenter)
 
         self.setLayout(layout)
         self.setFixedSize(400, 300)
