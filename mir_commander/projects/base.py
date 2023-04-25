f05b7a9ddd896a8650ec9086fdec9a2bdd4201b0
Yury V. Vishnevskiy <yu.v.vishnevskiy@gmail.com>
2023-04-25 10:04:32 +0200

    Added changes from main

--- mir_commander/projects/base.py
+++ mir_commander/projects/base.py
@@ -1,8 +1,17 @@
+import os
+
+from mir_commander.settings import Settings
 from mir_commander.utils.item import Item
 
 
 class Project:
     """The most basic class of projects."""
 
-    def __init__(self):
+    def __init__(self, path: str):
+        self.path = os.path.normpath(path)
+        self.settings = Settings(os.path.join(self.path, ".mircmd", "config"))
         self.root_item = Item("root")
+
+    @property
+    def title(self) -> str:
+        return os.path.split(self.path)[1] or "/"
