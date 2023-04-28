14263ef1fdf927070167f2bec17064b0ea215d28
Valery Vishnevskiy <v.v.vishnevskiy@yandex.ru>
2023-04-28 08:45:17 +0300

    Improved About widget

--- mir_commander/ui/utils/widget.py
+++ mir_commander/ui/utils/widget.py
@@ -1,4 +1,7 @@
-from PySide6.QtCore import QEvent
+from typing import Optional
+
+from PySide6.QtCore import QCoreApplication, QEvent
+from PySide6.QtWidgets import QDialog, QLabel, QWidget
 
 
 class Translator:
@@ -9,6 +12,13 @@ class Translator:
     must inherit this class.
     """
 
+    @staticmethod
+    def tr(value: str):
+        return value
+
+    def _tr(self, value: str) -> str:
+        return QCoreApplication.translate(self.__class__.__name__, value)
+
     def retranslate_ui(self):
         pass
 
@@ -18,3 +28,25 @@ class Translator:
         if event.type() == QEvent.LanguageChange:
             self.retranslate_ui()
         super().changeEvent(event)  # type: ignore
+
+
+class Dialog(Translator, QDialog):
+    def setWindowTitle(self, value: str):
+        self.__window_title = value
+        super().setWindowTitle(self._tr(value))
+
+    def retranslate_ui(self):
+        self.setWindowTitle(self.__window_title)
+
+
+class Label(Translator, QLabel):
+    def __init__(self, text: str, parent: Optional[QWidget] = None):
+        self.__text = text
+        super().__init__(self._tr(text), parent)
+
+    def setText(self, value: str):
+        self.__text = value
+        super().setText(self._tr(value))
+
+    def retranslate_ui(self):
+        self.setText(self.__text)
