8d9b730c4d1b9baf4e7ebe213b4be44cd66e5f50
NoviciusVulgaris <128837362+NoviciusVulgaris@users.noreply.github.com>
2023-05-25 20:04:50 +0200

    [MIR-73] Added Window menu (#20)

--- mir_commander/ui/main_window/__init__.py
+++ mir_commander/ui/main_window/__init__.py
@@ -1,11 +1,12 @@
 import base64
 import logging
 from dataclasses import dataclass
+from functools import partial
 from typing import TYPE_CHECKING
 
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, Slot
 from PySide6.QtGui import QCloseEvent, QIcon, QKeySequence
-from PySide6.QtWidgets import QMainWindow, QMdiArea, QTabWidget
+from PySide6.QtWidgets import QMainWindow, QMdiArea, QMdiSubWindow, QTabWidget
 
 from mir_commander import __version__
 from mir_commander.projects.base import Project
@@ -34,7 +35,7 @@ class MainWindow(QMainWindow):
         self.app: "Application" = app
         self.project = project
 
-        self.project.settings.add_apply_callback("name", self._set_window_title)
+        self.project.settings.add_apply_callback("name", self._set_mainwindow_title)
 
         self._config = self.project.config.nested("widgets.main_window")
 
@@ -44,6 +45,7 @@ class MainWindow(QMainWindow):
         self.mdi_area = QMdiArea(self)
         self.mdi_area.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
         self.mdi_area.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        self.mdi_area.subWindowActivated.connect(self.update_menus)
         self.setCentralWidget(self.mdi_area)
 
         self.setup_docks()
@@ -53,10 +55,12 @@ class MainWindow(QMainWindow):
         self.status = StatusBar(self)
         self.setStatusBar(self.status)
 
-        self._set_window_title()
+        self._set_mainwindow_title()
 
         # Settings
         self._restore_settings()
+
+        self.update_menus(None)
 
         self.status.showMessage(StatusBar.tr("Ready"), 10000)
         self.docks.console.append(self.tr("Started") + f" Mir Commander {__version__}")
@@ -79,13 +83,14 @@ class MainWindow(QMainWindow):
         self.addDockWidget(Qt.BottomDockWidgetArea, self.docks.console)
         self.docks.project.set_model(self.project.model)
 
-    def _set_window_title(self):
+    def _set_mainwindow_title(self):
         self.setWindowTitle(f"Mir Commander – {self.project.name}")
 
     def setup_menubar(self):
         menubar = self.menuBar()
         menubar.addMenu(self._setup_menubar_file())
         menubar.addMenu(self._setup_menubar_view())
+        menubar.addMenu(self._setup_menubar_window())
         menubar.addMenu(self._setup_menubar_help())
 
     def _setup_menubar_file(self) -> Menu:
@@ -101,6 +106,13 @@ class MainWindow(QMainWindow):
         menu.addAction(self.docks.object.toggleViewAction())
         menu.addAction(self.docks.console.toggleViewAction())
         return menu
+
+    def _setup_menubar_window(self) -> Menu:
+        self._window_actions()
+        self._window_menu = Menu(Menu.tr("&Window"), self)
+        self.update_window_menu()
+        self._window_menu.aboutToShow.connect(self.update_window_menu)
+        return self._window_menu
 
     def _setup_menubar_help(self) -> Menu:
         menu = Menu(Menu.tr("Help"), self)
@@ -132,6 +144,51 @@ class MainWindow(QMainWindow):
         action.triggered.connect(About(self).show)
         return action
 
+    def _window_actions(self):
+        self._win_close_act = Action(
+            Action.tr("Cl&ose"),
+            self,
+            statusTip=self.tr("Close the active window"),
+            triggered=self.mdi_area.closeActiveSubWindow,
+        )
+
+        self._win_close_all_act = Action(
+            Action.tr("Close &All"),
+            self,
+            statusTip=self.tr("Close all the windows"),
+            triggered=self.mdi_area.closeAllSubWindows,
+        )
+
+        self._win_tile_act = Action(
+            Action.tr("&Tile"), self, statusTip=self.tr("Tile the windows"), triggered=self.mdi_area.tileSubWindows
+        )
+
+        self._win_cascade_act = Action(
+            Action.tr("&Cascade"),
+            self,
+            statusTip=self.tr("Cascade the windows"),
+            triggered=self.mdi_area.cascadeSubWindows,
+        )
+
+        self._win_next_act = Action(
+            Action.tr("Ne&xt"),
+            self,
+            shortcut=QKeySequence.NextChild,
+            statusTip=self.tr("Move the focus to the next window"),
+            triggered=self.mdi_area.activateNextSubWindow,
+        )
+
+        self._win_previous_act = Action(
+            Action.tr("Pre&vious"),
+            self,
+            shortcut=QKeySequence.PreviousChild,
+            statusTip=self.tr("Move the focus to the previous window"),
+            triggered=self.mdi_area.activatePreviousSubWindow,
+        )
+
+        self._win_separator_act = Action(self)
+        self._win_separator_act.setSeparator(True)
+
     def view_opened_items(self):
         for item in self.project.opened_items:
             if viewer := item.viewer():
@@ -158,3 +215,47 @@ class MainWindow(QMainWindow):
         self._save_settings()
         self.app.close_project(self)
         event.accept()
+
+    @Slot()
+    def update_menus(self, window: QMdiSubWindow):
+        has_mdi_child = window is not None
+        self._win_close_act.setEnabled(has_mdi_child)
+        self._win_close_all_act.setEnabled(has_mdi_child)
+        self._win_tile_act.setEnabled(has_mdi_child)
+        self._win_cascade_act.setEnabled(has_mdi_child)
+        self._win_next_act.setEnabled(has_mdi_child)
+        self._win_previous_act.setEnabled(has_mdi_child)
+        self._win_separator_act.setVisible(has_mdi_child)
+
+    def set_active_sub_window(self, window: QMdiSubWindow) -> None:
+        if window:
+            self.mdi_area.setActiveSubWindow(window)
+
+    @Slot()
+    def update_window_menu(self):
+        self._window_menu.clear()
+        self._window_menu.addAction(self._win_close_act)
+        self._window_menu.addAction(self._win_close_all_act)
+        self._window_menu.addSeparator()
+        self._window_menu.addAction(self._win_tile_act)
+        self._window_menu.addAction(self._win_cascade_act)
+        self._window_menu.addSeparator()
+        self._window_menu.addAction(self._win_next_act)
+        self._window_menu.addAction(self._win_previous_act)
+        self._window_menu.addAction(self._win_separator_act)
+
+        windows = self.mdi_area.subWindowList()
+        active_sub_window = self.mdi_area.activeSubWindow()
+        self._win_separator_act.setVisible(len(windows) != 0)
+
+        for i, window in enumerate(windows):
+            f = window.windowTitle()
+            text = f"{i + 1} {f}"
+            if i < 9:
+                text = "&" + text
+
+            action = self._window_menu.addAction(text)
+            action.setCheckable(True)
+            action.setChecked(window is active_sub_window)
+            slot_func = partial(self.set_active_sub_window, window=window)
+            action.triggered.connect(slot_func)
