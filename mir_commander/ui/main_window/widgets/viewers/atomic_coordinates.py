608bd9f3e3dd0ef81f60f09c0c77a9ca14e544ea
Valery Vishnevskiy <v.v.vishnevskiy@yandex.ru>
2023-05-26 14:52:51 +0300

    Improvements

--- mir_commander/ui/main_window/widgets/viewers/atomic_coordinates.py
+++ mir_commander/ui/main_window/widgets/viewers/atomic_coordinates.py
@@ -1,6 +1,3 @@
-from PySide6.QtCore import QKeyCombination, Qt
-from PySide6.QtGui import QKeyEvent
-
 from mir_commander.ui.main_window.widgets.viewers.molecule import Molecule
 
 
@@ -8,20 +5,8 @@ class AtomicCoordinates(Molecule):
     def _set_draw_item(self):
         self._draw_item = self.item
 
-    def _key_press_handler(self, event: QKeyEvent) -> bool:
-        ctrl_up = {
-            QKeyCombination(Qt.ControlModifier | Qt.KeypadModifier, Qt.Key_Up),
-            QKeyCombination(Qt.ControlModifier, Qt.Key_Up),
-        }
-        ctrl_down = {
-            QKeyCombination(Qt.ControlModifier | Qt.KeypadModifier, Qt.Key_Down),
-            QKeyCombination(Qt.ControlModifier, Qt.Key_Down),
-        }
-        if event.keyCombination() in ctrl_up:  # Ctrl + Up
-            self._set_prev_style()
-        elif event.keyCombination() in ctrl_down:  # Ctrl + Down
-            self._set_next_style()
-        else:
-            # No match
-            return False  # not processed
-        return True  # processed
+    def _draw_next_item(self):
+        pass
+
+    def _draw_prev_item(self):
+        pass
