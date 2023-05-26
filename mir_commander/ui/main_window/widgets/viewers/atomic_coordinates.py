f556dfa718eec8d8c9e1f25b8aab89bbe14f935a
Valery Vishnevskiy <v.v.vishnevskiy@yandex.ru>
2023-05-26 14:28:16 +0300

    [MIR-77] Fixed changing style for molecule viewer when we open certain AtomicCoordinates item

--- mir_commander/ui/main_window/widgets/viewers/atomic_coordinates.py
+++ mir_commander/ui/main_window/widgets/viewers/atomic_coordinates.py
@@ -1,3 +1,6 @@
+from PySide6.QtCore import QKeyCombination, Qt
+from PySide6.QtGui import QKeyEvent
+
 from mir_commander.ui.main_window.widgets.viewers.molecule import Molecule
 
 
@@ -5,5 +8,20 @@ class AtomicCoordinates(Molecule):
     def _set_draw_item(self):
         self._draw_item = self.item
 
-    def _key_press_handler(self, *args, **kwargs) -> bool:
-        return False
+    def _key_press_handler(self, event: QKeyEvent) -> bool:
+        ctrl_up = {
+            QKeyCombination(Qt.ControlModifier | Qt.KeypadModifier, Qt.Key_Up),
+            QKeyCombination(Qt.ControlModifier, Qt.Key_Up),
+        }
+        ctrl_down = {
+            QKeyCombination(Qt.ControlModifier | Qt.KeypadModifier, Qt.Key_Down),
+            QKeyCombination(Qt.ControlModifier, Qt.Key_Down),
+        }
+        if event.keyCombination() in ctrl_up:  # Ctrl + Up
+            self._set_prev_style()
+        elif event.keyCombination() in ctrl_down:  # Ctrl + Down
+            self._set_next_style()
+        else:
+            # No match
+            return False  # not processed
+        return True  # processed
