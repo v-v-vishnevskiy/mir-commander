5ea6dbd51db79971779f2e78dcbe2bb70e37ef24
Valery Vishnevskiy <v.v.vishnevskiy@yandex.ru>
2023-10-18 21:24:51 +0300

    Improved default fragment shader

--- mir_commander/ui/utils/opengl/default_shaders.py
+++ mir_commander/ui/utils/opengl/default_shaders.py
@@ -11,13 +11,30 @@ SHADED = {
     "fragment": """
         varying vec3 normal;
         void main() {
-            float p = dot(normal, normalize(vec3(1.0, 1.0, 1.0)));
-            p = p < 0. ? 0. : p * 0.8;
+            vec3 light_color = vec3(1.0, 1.0, 1.0) * 0.9;
+            vec3 light_pos = vec3(0.3, 0.3, 1.0);
+
+            // Ambient
+            float ambient_strength = 0.1;
+            vec3 ambient = light_color * ambient_strength;
+
+            // Diffuse
+            vec3 norm = normalize(normal);
+            vec3 light_dir = normalize(light_pos);
+            float diff = max(dot(norm, light_dir), 0.0);
+            vec3 diffuse = diff * light_color;
+
+            // Specular
+            float specular_strength = 1.0;
+            vec3 FragPos = gl_FragCoord.xyz;
+            vec3 viewDir = normalize(light_pos);
+            vec3 reflectDir = reflect(-light_dir, norm);
+            float spec = pow(max(dot(viewDir, reflectDir), 0.0), 16.0);
+            vec3 specular = specular_strength * spec * light_color;
+
             vec4 color = gl_Color;
-            color.x = color.x * (0.2 + p);
-            color.y = color.y * (0.2 + p);
-            color.z = color.z * (0.2 + p);
-            gl_FragColor = color;
+            vec3 p = ((ambient + diffuse) * color.xyz) + specular;
+            gl_FragColor = vec4(p, color.w);
         }
     """,
 }
