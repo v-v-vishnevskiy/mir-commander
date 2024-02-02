ccac960d38356921057afcb8818b5d5883474c6e
Yury V. Vishnevskiy <yu.v.vishnevskiy@gmail.com>
2024-02-02 13:22:30 +0100

    Starting script can be run from everywhere

--- dev_run.sh
+++ dev_run.sh
@@ -1,5 +1,38 @@
 #!/usr/bin/env bash
 
-source .venv/bin/activate
+sargs=("$@")
 
-PYTHONPATH=. python mir_commander "$@"
+currentDir=$(pwd)
+thisDirName=$(dirname "$0")
+cd "$thisDirName" || exit 1
+mircmdDir=$(pwd)
+cd "$currentDir" || exit 1
+
+if [ -z "$MIRCMD_ROOT_DIR" ]; then
+    export MIRCMD_ROOT_DIR=$mircmdDir
+fi
+
+
+source "${MIRCMD_ROOT_DIR}/.venv/bin/activate"
+
+
+declare -a uargs
+for ((i=0; i < ${#sargs[@]}; i++))
+do
+    item=${sargs[i]}
+    case "${item}" in
+        --wrapper)
+            ((i++))
+            wrapper="${sargs[i]}"
+            ;;
+        --wrapper=*)
+            wrapper="${item#*=}"
+            ;;
+        *)
+            uargs[${#uargs[@]}]="${item}"
+            ;;
+    esac
+done
+
+
+PYTHONPATH="${MIRCMD_ROOT_DIR}" $wrapper python "$MIRCMD_ROOT_DIR/mir_commander" "${uargs[@]}"
