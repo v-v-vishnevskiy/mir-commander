eaa939f544668ed11168dc3b6dd8dbf3427a42ae
Yury V. Vishnevskiy <yu.v.vishnevskiy@yandex.ru>
2024-04-13 11:40:39 +0200

    Code refactoring

--- mir_commander/ui/utils/opengl/scene.py
+++ mir_commander/ui/utils/opengl/scene.py
@@ -203,6 +203,52 @@ class Scene:
     def update(self):
         self._gl_widget.update()
 
+    def crop_image_to_content(self, image: QImage, bg_color: QColor) -> QImage:
+        # Need this hack with the fake 1x1 image to take into account the format of our real image
+        # so we know the value of the background color as it is represented in the image.
+        bg_image = QImage(1, 1, image.format())
+        bg_image.setPixelColor(0, 0, bg_color)
+        bg_color_value = bg_image.pixel(0, 0)
+        xmin = ymin = xmax = ymax = -1
+
+        for y in range(image.height()):
+            for x in range(image.width()):
+                if image.pixel(x, y) != bg_color_value:
+                    ymin = y
+                    break
+            if ymin >= 0:
+                break
+
+        for y in reversed(range(image.height())):
+            for x in range(image.width()):
+                if image.pixel(x, y) != bg_color_value:
+                    ymax = y
+                    break
+            if ymax >= 0:
+                break
+
+        for x in range(image.width()):
+            for y in range(image.height()):
+                if image.pixel(x, y) != bg_color_value:
+                    xmin = x
+                    break
+            if xmin >= 0:
+                break
+
+        for x in reversed(range(image.width())):
+            for y in range(image.height()):
+                if image.pixel(x, y) != bg_color_value:
+                    xmax = x
+                    break
+            if xmax >= 0:
+                break
+
+        if xmin >= 0 and xmax >= 0 and ymin >= 0 and ymax >= 0:
+            crop_area = QRect(xmin, ymin, xmax - xmin + 1, ymax - ymin + 1)
+            image = image.copy(crop_area)
+
+        return image
+
     def render_to_image(
         self, width: int, height: int, transparent_bg: bool = False, crop_to_content: bool = False
     ) -> QImage:
@@ -237,47 +283,6 @@ class Scene:
             image = image.convertToFormat(QImage.Format_RGB32)
 
         if crop_to_content:
-            # Need this hack with the fake 1x1 image to take into account the format of our real image
-            # so we know the value of the background color as it is represented in the image.
-            bg_image = QImage(1, 1, image.format())
-            bg_image.setPixelColor(0, 0, bg_color)
-            bg_color_value = bg_image.pixel(0, 0)
-            xmin = ymin = xmax = ymax = -1
-
-            for y in range(image.height()):
-                for x in range(image.width()):
-                    if image.pixel(x, y) != bg_color_value:
-                        ymin = y
-                        break
-                if ymin >= 0:
-                    break
-
-            for y in reversed(range(image.height())):
-                for x in range(image.width()):
-                    if image.pixel(x, y) != bg_color_value:
-                        ymax = y
-                        break
-                if ymax >= 0:
-                    break
-
-            for x in range(image.width()):
-                for y in range(image.height()):
-                    if image.pixel(x, y) != bg_color_value:
-                        xmin = x
-                        break
-                if xmin >= 0:
-                    break
-
-            for x in reversed(range(image.width())):
-                for y in range(image.height()):
-                    if image.pixel(x, y) != bg_color_value:
-                        xmax = x
-                        break
-                if xmax >= 0:
-                    break
-
-            if xmin >= 0 and xmax >= 0 and ymin >= 0 and ymax >= 0:
-                crop_area = QRect(xmin, ymin, xmax - xmin + 1, ymax - ymin + 1)
-                image = image.copy(crop_area)
+            image = self.crop_image_to_content(image, bg_color)
 
         return image
