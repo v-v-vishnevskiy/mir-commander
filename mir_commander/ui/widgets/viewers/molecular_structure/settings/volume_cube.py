e21605be7a48f1b2312fbea2c73278661012cccc
Valery Vishnevskiy <v.v.vishnevskiy@yandex.ru>
2025-10-02 13:09:34 +0300

    Added list of added surfaces and remove button

--- mir_commander/ui/widgets/viewers/molecular_structure/settings/volume_cube.py
+++ mir_commander/ui/widgets/viewers/molecular_structure/settings/volume_cube.py
@@ -1,7 +1,7 @@
 from typing import TYPE_CHECKING
 
-from PySide6.QtGui import QColor, QMouseEvent
-from PySide6.QtWidgets import QColorDialog, QDoubleSpinBox, QFrame, QGridLayout, QVBoxLayout, QWidget
+from PySide6.QtGui import QColor, QMouseEvent, QPixmap
+from PySide6.QtWidgets import QColorDialog, QComboBox, QDoubleSpinBox, QFrame, QGridLayout, QVBoxLayout, QWidget
 
 from mir_commander.ui.utils.opengl.utils import color_to_qcolor
 from mir_commander.ui.utils.widget import GroupBox, PushButton
@@ -47,7 +47,6 @@ class VolumeCube(GroupBox):
         self._value.setSingleStep(0.01)
         self._value.setDecimals(5)
         self._value.setValue(0.0)
-        # self.value_spinbox.valueChanged.connect(self.value_spinbox_changed_handler)
 
         # Value layout
         value_layout = QGridLayout()
@@ -57,9 +56,19 @@ class VolumeCube(GroupBox):
         add_button = PushButton(PushButton.tr("Add"), parent)
         add_button.clicked.connect(self.add_button_clicked_handler)
 
+        self._surface_combo_box = QComboBox()
+        self._surface_combo_box.setInsertPolicy(QComboBox.InsertPolicy.InsertAlphabetically)
+        self._surface_combo_box.setDisabled(True)
+
+        self._remove_button = PushButton(PushButton.tr("Remove"), parent)
+        self._remove_button.setDisabled(True)
+        self._remove_button.clicked.connect(self.remove_button_clicked_handler)
+
         value_layout.addWidget(self._value, 0, 0)
         value_layout.addWidget(self._color_button, 0, 1)
         value_layout.addWidget(add_button, 0, 2)
+        value_layout.addWidget(self._surface_combo_box, 1, 0, 1, 2)
+        value_layout.addWidget(self._remove_button, 1, 2)
 
         self.main_layout = QVBoxLayout()
         self.main_layout.addLayout(value_layout)
@@ -80,10 +89,6 @@ class VolumeCube(GroupBox):
         for viewer in viewers:
             viewer.visualizer.add_volume_cube_surface(value=value, color=color)
 
-    # def value_spinbox_changed_handler(self, value: float):
-    #     for viewer in self._settings.viewers:
-    #         viewer.visualizer.add_volume_cube(value=value)
-
     def add_button_clicked_handler(self):
         value = self._value.value()
         color = (
@@ -94,3 +99,27 @@ class VolumeCube(GroupBox):
         )
         for viewer in self._settings.viewers:
             viewer.visualizer.add_volume_cube_surface(value=value, color=color)
+
+        pixmap = QPixmap(20, 20)
+        pixmap.fill(self._color_button.color)
+        index = self._surface_combo_box.findText(str(value))
+        if index == -1:
+            self._surface_combo_box.addItem(pixmap, str(value))
+        else:
+            self._surface_combo_box.setItemIcon(index, pixmap)
+
+        self._surface_combo_box.setDisabled(False)
+        self._remove_button.setDisabled(False)
+
+    def remove_button_clicked_handler(self):
+        if self._surface_combo_box.count() == 0:
+            return
+
+        value = self._surface_combo_box.currentText()
+        for viewer in self._settings.viewers:
+            viewer.visualizer.remove_volume_cube_surface(value=float(value))
+        self._surface_combo_box.removeItem(self._surface_combo_box.currentIndex())
+
+        if self._surface_combo_box.count() == 0:
+            self._surface_combo_box.setDisabled(True)
+            self._remove_button.setDisabled(True)
